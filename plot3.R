e6f627be9c15154b74041561cde1e0ba65462824
Daniel Ambrosio <daniel.ambrosio@gmail.com>
2015-04-11 21:40:31 -0300

    fixed the legend truncate

--- plot3.R
+++ plot3.R
@@ -13,6 +13,7 @@ setwd(this.dir)
 if(!exists("subset_data")) source("get_file.R")
 
 par(mfrow=c(1,1), mar=c(4,4,4,4), oma=c(0,0,0,0))
+png(filename = "plot3.png", width=480, height=480)
 
 plot(subset_data$Datetime, subset_data$Sub_metering_1, type="l", xlab="", 
      ylab="Energy Sub Metering")
@@ -22,7 +23,7 @@ lines(subset_data$Datetime, subset_data$Sub_metering_3, col = "blue")
 legend("topright", col=c("black", "red", "blue"), lty=1, lwd=2, xjust=1,
        legend=c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3"))
 
-dev.copy(png, file="plot3.png", height=480, width=480)
+#dev.copy(png, file="plot3.png", height=480, width=480)
 dev.off()
 
 # go back to old working directory
